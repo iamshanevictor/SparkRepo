e98fa5970025fc155595e6accbf1014208df36ce
Shane Victor <batoonshanevictor@yahoo.com>
2025-08-25 08:20:32 +0800

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:5000/api')
-  }
+  plugins: [vue()]
 })
